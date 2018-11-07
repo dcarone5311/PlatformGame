85224933b72e27ac12fd0107a0a6f33ef9d828e2
MYOOOTT <mnguy1231@gmail.com>
2018-11-07 15:48:29 -0800

    Implemented timer & added own scene.

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    private float seconds;
+
+    public Text text;
+
+    void Awake()
+    {
+        //make the timer start at 0
+        seconds = 0;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        seconds += Time.deltaTime; //deltaTime = "seconds it took to complete the last frame"
+        text.text = seconds.ToString();
+
+
+	}
+}
