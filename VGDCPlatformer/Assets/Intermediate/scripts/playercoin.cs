3f1bd684214ad7a1d11cf3d3682f52868f553f93
Jessica Sill <jsill@uci.edu>
2018-11-09 15:17:54 -0800

    added coins
    pretty ghetto so far.

---
+++ VGDCPlatformer/Assets/Intermediate/scripts/playercoin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercoin : MonoBehaviour
+{
+
+    private coincount coincount1;
+
+    // Use this for initialization
+    void Start()
+    {
+        coincount1 = GameObject.FindWithTag("coincount").GetComponent<coincount>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("coin"))
+        {
+            Destroy(col.gameObject);
+            coincount1.coins += 1;
+        }
+    }
+}
