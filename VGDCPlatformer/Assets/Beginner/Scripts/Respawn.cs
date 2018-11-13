3b3856b1b83801c9943797200abb85e94678d801
MYOOOTT <mnguy1231@gmail.com>
2018-11-12 21:03:12 -0800

    Respawn script & setup.

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/Respawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onTriggerEnter2D(Collider collider)
+    {
+        Debug.Log("Respawn point triggered.");
+        player.transform.position = respawnPoint.transform.position;
+    }
+
+    void onCollisionEnter2D(Collider collider)
+    {
+        Debug.Log("Respawn point triggered. (Collision)");
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
