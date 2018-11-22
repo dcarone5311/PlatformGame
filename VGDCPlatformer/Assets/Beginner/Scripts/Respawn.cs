291adc1d9f471e525fdad142df46a08773495147
Jessica Sill <jsill@uci.edu>
2018-11-22 10:05:19 -0800

    added very simple lives sytem.
    piggybacks off of the respawn script

--- VGDCPlatformer/Assets/Beginner/Scripts/Respawn.cs
+++ VGDCPlatformer/Assets/Beginner/Scripts/Respawn.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Respawn : MonoBehaviour {
 
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
+    private int lives;
+    public Text livesText;
+    
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        lives = 3;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        livesText.text = ("Lives: " + lives);
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -23,6 +30,17 @@ public class Respawn : MonoBehaviour {
         player.transform.rotation = Quaternion.identity;
         //player.transform.Rotate(Vector3.up, 360 - Mathf.Abs(player.transform.rotation.z));
         player.transform.position = respawnPoint.transform.position;
+        lives -= 1;
+        if (lives <= 0)
+        {
+            gameOver();
+        }
+    }
+
+    // reloads the level
+    void gameOver()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 
         
